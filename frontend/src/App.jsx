9dac7ad999a3efedcae18066b96893f93abe74b5
Sandeep-mehta-IITP <sandeep797671@gmail.com>
2025-06-04 09:36:19 +0530

    refactor: replace auth query with custom hook and add loading state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,26 +7,16 @@ import Notification from "./pages/Notification.jsx";
 import Call from "./pages/Call.jsx";
 import Chat from "./pages/Chat.jsx";
 import { Toaster } from "react-hot-toast";
-import { useQuery } from "@tanstack/react-query";
-import { axiosInstance } from "./lib/axios.js";
+import PageLoader from "./components/PageLoader.jsx";
+import useAuthUser from "./hooks/useAuthUser.js";
 
 function App() {
   // tanstack query
 
-  const {
-    data: authData,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["authUser"],
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-    retry: false, // auth check
-  });
+  const { isLoading, authUser} = useAuthUser ();
+  
 
-  const authUser = authData?.user;
+  if (isLoading) return < PageLoader />;
 
   return (
     <div className="h-screen" data-theme="night">
