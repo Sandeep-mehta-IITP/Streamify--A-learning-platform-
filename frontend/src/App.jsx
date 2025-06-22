b0ded2d18bece604e4f53b0a6f771e16afae543b
Sandeep Mehta <sandeep797671@gmail.com>
2025-06-22 19:25:09 +0530

    feat: add theme management with Zustand and implement ThemeSelector component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,9 +10,11 @@ import { Toaster } from "react-hot-toast";
 import PageLoader from "./components/PageLoader.jsx";
 import useAuthUser from "./hooks/useAuthUser.js";
 import Layout from "./components/Layout.jsx";
+import { useThemeStore } from "./utils/useThemeStore.js";
 
 function App() {
   const { isLoading, authUser } = useAuthUser();
+  const { theme } = useThemeStore();
 
   const isAuthenticated = Boolean(authUser);
   const isOnboarded = authUser?.isOnboarded;
@@ -20,7 +22,7 @@ function App() {
   if (isLoading) return <PageLoader />;
 
   return (
-    <div className="h-screen" data-theme="night">
+    <div className="h-screen" data-theme={theme}>
       <Routes>
         <Route
           path="/"
--- frontend/src/components/ThemeSelector.jsx
+++ frontend/src/components/ThemeSelector.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useThemeStore } from "../utils/useThemeStore";
+import { PaletteIcon } from "lucide-react";
+import { THEMES } from "../constants";
 
 const ThemeSelector = () => {
+  const { theme, setTheme } = useThemeStore();
   return (
-    <div>ThemeSelector</div>
-  )
-}
+    <div className="dropdown dropdown-end">
+      {/*----DROPDOWN TRIGGER-----*/}
+      <button tabIndex={0} className="btn btn-ghost btn-circle">
+        <PaletteIcon className="size-5" />
+      </button>
 
-export default ThemeSelector
+      <div
+        tabIndex={0}
+        className="dropdown-content mt-2 p-1 shadow-2xl bg-base-200 backdrop-blur-lg rounded-2xl w-56 border border-base-content/10 max-h-80 overflow-y-auto"
+      >
+        <div className="space-y-1">
+          {THEMES.map((item) => (
+            <button
+              key={item.name}
+              className={`w-full px-4 py-3 rounded-xl flex items-center gap-3 transition-colors
+                ${
+                  theme === item.name
+                    ? "bg-primary/10 text-primary"
+                    : "hover:bg-base-content/5"
+                }`}
+              onClick={() => setTheme(item.name)}
+            >
+              <PaletteIcon className="size-4" />
+              <span className="text-sm font-medium">{item.label}</span>
+
+              {/*-----THEME PREVIEW COLORS--------*/}
+              <div className="ml-auto flex gap-1">
+                {item.colors.map((color, idx) => (
+                  <span
+                    key={idx}
+                    className="size-2 rounded-full"
+                    style={{ backgroundColor: color }}
+                  ></span>
+                ))}
+              </div>
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ThemeSelector;
